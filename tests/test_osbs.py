23952cf2877673c21bde894cbce5644cab5b128e
Tomas Tomecek <ttomecek@redhat.com>
2015-09-17 11:47:13 +0200

    test if version in spec, setup.py and __version__ matches

---
+++ tests/test_osbs.py
@@ -0,0 +1,33 @@
+"""
+Copyright (c) 2015 Red Hat, Inc
+All rights reserved.
+
+This software may be modified and distributed under the terms
+of the BSD license. See the LICENSE file for details.
+"""
+from __future__ import print_function, unicode_literals
+
+import os
+import re
+import inspect
+
+
+def test_if_all_versions_match():
+    def read_version(fp, regex):
+        with open(fp, "r") as fd:
+            content = fd.read()
+            found = re.findall(regex, content)
+            if len(found) == 1:
+                return found[0]
+            else:
+                raise Exception("Version not found!")
+    import osbs
+    from osbs import __version__
+    fp = inspect.getfile(osbs)
+    project_dir = os.path.dirname(os.path.dirname(fp))
+    specfile = os.path.join(project_dir, "osbs-client.spec")
+    setup_py = os.path.join(project_dir, "setup.py")
+    spec_version = read_version(specfile, r"\nVersion:\s*(.+?)\s*\n")
+    setup_py_version = read_version(setup_py, r"version=['\"](.+)['\"]")
+    assert spec_version == __version__
+    assert setup_py_version == __version__
