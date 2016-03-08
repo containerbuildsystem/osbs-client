a3ebe6ab0f2a2ee8a4b79991d1c96138a3129d5e
Vadim Rutkovsky <vrutkovs@redhat.com>
2016-03-08 14:23:15 +0100

    WIP: Fix authentication

--- tests/mock_jsons.py
+++ tests/mock_jsons.py
@@ -12,6 +12,10 @@ httpd_auth_url = None
 os_osapi_url = None
 os_auth_url = None
 
+os_build = os.getenv("OS_BUILD", "")
+os_username = os.getenv("OS_USERNAME", "")
+os_password = os.getenv("OS_PASSWORD", "")
+
 os_proxy_url = os.getenv("OS_PROXY_URL", None)
 if os_proxy_url:
     if int(os_version.split(".")[0]) > 0:
@@ -25,9 +29,9 @@ if os_proxy_url:
 os_url = os.getenv("OS_URL", None)
 if os_url:
     if int(os_version.split(".")[0]) > 0:
-        os_osapi_url = "{}/osapi/v3beta".format(os_url)
-    else:
         os_osapi_url = "{}/oapi/v1".format(os_url)
+    else:
+        os_osapi_url = "{}/osapi/v3beta".format(os_url)
     os_auth_url = "{}/oauth/authorize?client_id={}&response_type=token".format(
         os_url, "openshift-challenging-client")
 
@@ -37,9 +41,15 @@ if not os.path.exists(store_path):
     os.makedirs(store_path)
 
 
-def request_and_save_output_to(url, prefix, request_type='get', headers={}, data={}):
-    request_options = {"verify_ssl": False, }
+def request_and_save_output_to(url, prefix, request_type='get',
+                               headers={}, data={}, auth_only=False, use_json=False):
+    request_options = {"verify_ssl": False, "use_json": use_json}
+    if auth_only:
+        request_options.update({
+            "username": os_username, "password": os_password, "allow_redirects": False
+        })
     connection = HttpSession(verbose=True)
+    print(url)
     r = {
         'get': connection.get,
         'put': connection.put
@@ -52,24 +62,34 @@ def request_and_save_output_to(url, prefix, request_type='get', headers={}, data
     file_name = "{}.{}".format(prefix, ext)
     file_path = os.path.join(store_path, file_name)
     content = r.content
+
     if ext == 'json':
-        content = json.dumps(r.json(), indent=4, separators=(',', ': '))
+        try:
+            content = json.dumps(r.json(), indent=4, separators=(',', ': '))
+        except Exception:
+            pass
 
     with open(file_path, 'w') as outfile:
         outfile.write(content)
+
+    if auth_only:
+        return str(r.headers)
 
     return content
 
 
 def get_access_token(prefix):
-    if not httpd_auth_url:
-        return "5pk5dWSU2KIsGxMsh6RZGkY6aFKGXCWp1ohkBPCuVt8"
-
-    output = request_and_save_output_to(httpd_auth_url, prefix, headers={'X-CSRF-Token': 'aaaa'})
-    regexp = r"access_token=([^&]+)"
+    url = None
+    if httpd_auth_url:
+        url = httpd_auth_url
+    else:
+        url = os_auth_url
+    output = request_and_save_output_to(url, prefix, auth_only=True)
+    regexp = r"access_token=[^&]+"
     return re.search(regexp, output).group(0)
 
 access_token = get_access_token("authorize")
+print("Got token: {}".format(access_token))
 
 if os_osapi_url:
     url = "{}/users/~?{}".format(os_osapi_url, access_token)
@@ -78,11 +98,8 @@ if os_osapi_url:
     url = "{}/namespaces/default/builds".format(os_osapi_url)
     request_and_save_output_to(url, "builds_list")
 
-    os_build = os.getenv("OS_BUILD", "")
-    url = "{}/namespaces/default/build/{}".format(os_osapi_url, os_build)
+    url = "{}/namespaces/default/builds/{}".format(os_osapi_url, os_build)
     build_json = request_and_save_output_to(url, "build_{}".format(os_build))
 
-    request_and_save_output_to(
-        url, "build_put", request_type="put",
-        headers={"Expect": "", "Content-Type": "application/json"},
-        data=json.loads(build_json))
+    build_data = json.loads(build_json)
+    request_and_save_output_to(url, "build_put", request_type="put", use_json=True, data=build_data)
