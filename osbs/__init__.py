f7dc1814a19f9a84ff7d2a27c2d119057793544c
Luiz Carvalho <lucarval@redhat.com>
2016-06-24 10:42:45 -0400

    0.27 release

--- osbs/__init__.py
+++ osbs/__init__.py
@@ -10,7 +10,7 @@ from __future__ import print_function, absolute_import, unicode_literals
 import logging
 
 
-__version__ = "0.26"
+__version__ = "0.27"
 
 
 def set_logging(name="osbs", level=logging.DEBUG):
