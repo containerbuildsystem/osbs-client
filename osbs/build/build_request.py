ee1ba46655d914e8bd9a1b593f1f711c3f870982
Luiz Carvalho <lucarval@redhat.com>
2017-01-17 10:23:34 -0500

    Remove support for update_parent_image_stream_tag plugin

--- osbs/build/build_request.py
+++ osbs/build/build_request.py
@@ -215,21 +215,6 @@ class BuildRequest(object):
                                           "add_yum_repo_by_url", "inject_proxy",
                                           self.spec.proxy.value)
 
-    def render_update_parent_image_stream_tag(self, use_auth=None):
-        phase = 'prebuild_plugins'
-        plugin = 'update_parent_image_stream_tag'
-        if not self.dj.dock_json_has_plugin_conf(phase, plugin):
-            return
-
-        self.dj.dock_json_set_arg(phase, plugin, 'openshift_url',
-                                  self.spec.builder_openshift_url.value)
-        self.dj.dock_json_set_arg(phase, plugin, 'build_json_dir',
-                                  self.spec.builder_build_json_dir.value)
-        self.dj.dock_json_set_arg(phase, plugin, 'image_stream_tag',
-                                  self.spec.trigger_imagestreamtag.value)
-        if use_auth is not None:
-            self.dj.dock_json_set_arg(phase, plugin, 'use_auth', use_auth)
-
     def render_check_and_set_rebuild(self, use_auth=None):
         if self.dj.dock_json_has_plugin_conf('prebuild_plugins',
                                              'check_and_set_rebuild'):
@@ -394,7 +379,6 @@ class BuildRequest(object):
         remove_plugins = [
             ("prebuild_plugins", "check_and_set_rebuild"),
             ("prebuild_plugins", "stop_autorebuild_if_disabled"),
-            ("prebuild_plugins", "update_parent_image_stream_tag"),
             ("exit_plugins", "sendmail"),
         ]
 
@@ -794,7 +778,6 @@ class BuildRequest(object):
         self.render_add_yum_repo_by_url()
 
         use_auth = self.spec.use_auth.value
-        self.render_update_parent_image_stream_tag(use_auth=use_auth)
         self.render_check_and_set_rebuild(use_auth=use_auth)
         self.render_store_metadata_in_osv3(use_auth=use_auth)
 
