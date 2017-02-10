61b8ae2d2a146f6b1151d5656fbcd5a410d9fced
Vadim Rutkovsky <vrutkovs@redhat.com>
2017-02-10 15:47:48 +0100

    0.34.1 release

--- osbs/__init__.py
+++ osbs/__init__.py
@@ -10,7 +10,7 @@ from __future__ import print_function, absolute_import, unicode_literals
 import logging
 
 
-__version__ = "0.34"
+__version__ = "0.34.1"
 
 
 def set_logging(name="osbs", level=logging.DEBUG):
