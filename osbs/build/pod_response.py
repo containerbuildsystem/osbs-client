1d6efc77f6a864582962330562fe70ad6e0ac363
Tim Waugh <twaugh@redhat.com>
2017-07-07 11:43:24 +0100

    New PodResponse.get_failure_reason() method
    Signed-off-by: Tim Waugh <twaugh@redhat.com>

--- osbs/build/pod_response.py
+++ osbs/build/pod_response.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) 2015 Red Hat, Inc
+Copyright (c) 2015, 2017 Red Hat, Inc
 All rights reserved.
 
 This software may be modified and distributed under the terms
@@ -50,3 +50,60 @@ class PodResponse(object):
         return dict([(status['image'], remove_prefix(status['imageID'],
                                                      'docker://'))
                      for status in statuses])
+
+    def get_failure_reason(self):
+        """
+        Find the reason a pod failed
+
+        :return: dict, which will always have key 'reason':
+                 reason: brief reason for state
+                 containerID (if known): ID of container
+                 exitCode (if known): numeric exit code
+        """
+
+        REASON_KEY = 'reason'
+        CID_KEY = 'containerID'
+        EXIT_KEY = 'exitCode'
+
+        pod_status = self.json.get('status', {})
+        statuses = pod_status.get('containerStatuses', [])
+
+        # Find the first non-zero exit code from a container
+        # and return its 'message' or 'reason' value
+        for status in statuses:
+            try:
+                terminated = status['state']['terminated']
+                exit_code = terminated['exitCode']
+                if exit_code != 0:
+                    reason_dict = {
+                        EXIT_KEY: exit_code,
+                    }
+
+                    if 'containerID' in terminated:
+                        reason_dict[CID_KEY] = terminated['containerID']
+
+                    for key in ['message', 'reason']:
+                        try:
+                            reason_dict[REASON_KEY] = terminated[key]
+                            break
+                        except KeyError:
+                            continue
+                    else:
+                        # Both 'message' and 'reason' are missing
+                        reason_dict[REASON_KEY] = 'Exit code {code}'.format(
+                            code=exit_code
+                        )
+
+                    return reason_dict
+            except KeyError:
+                continue
+
+        # Failing that, return the 'message' or 'reason' value for the
+        # pod
+        for key in ['message', 'reason']:
+            try:
+                return {REASON_KEY: pod_status[key]}
+            except KeyError:
+                continue
+
+        return {REASON_KEY: pod_status['phase']}
