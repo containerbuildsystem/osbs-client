20a7363bad889340e6047a75f5d7454252155c1d
Luiz Carvalho <lucarval@redhat.com>
2018-04-16 15:15:31 -0400

    Run check_and_set_rebuild plugin earlier
    This allows check_and_set_rebuild plugin to modify the current git
    commit before other plugins read from git clone.
    
    Signed-off-by: Luiz Carvalho <lucarval@redhat.com>

--- tests/build_/test_arrangements.py
+++ tests/build_/test_arrangements.py
@@ -1388,6 +1388,7 @@ class TestArrangementV6(ArrangementBase):
         ORCHESTRATOR_INNER_TEMPLATE: {
             'prebuild_plugins': [
                 'reactor_config',
+                'check_and_set_rebuild',
                 'check_and_set_platforms',
                 'resolve_module_compose',
                 'flatpak_create_dockerfile',
@@ -1397,7 +1398,6 @@ class TestArrangementV6(ArrangementBase):
                 'bump_release',
                 'add_labels_in_dockerfile',
                 'koji_parent',
-                'check_and_set_rebuild',
                 'resolve_composes',
             ],
 
