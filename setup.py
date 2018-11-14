a7613a85ea627b5f87c3bd160b44e8ebb2f2734c
Robert Cerven <rcerven@redhat.com>
2018-11-14 16:28:40 +0100

    0.52 release
    Signed-off-by: Robert Cerven <rcerven@redhat.com>

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def _install_requirements():
 setup(
     name="osbs-client",
     description='Python module and command line client for OpenShift Build Service',
-    version="0.52.dev",
+    version="0.52",
     author='Red Hat, Inc.',
     author_email='atomic-devel@projectatomic.io',
     url='https://github.com/projectatomic/osbs-client',
