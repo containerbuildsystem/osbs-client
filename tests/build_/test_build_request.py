1b723bca8491087bf7a8d303ab652af8bad3d8ff
Adam Cmiel <acmiel@redhat.com>
2019-04-11 10:53:13 +0200

    Fix print-statement error
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- tests/build_/test_build_request.py
+++ tests/build_/test_build_request.py
@@ -5,7 +5,7 @@ All rights reserved.
 This software may be modified and distributed under the terms
 of the BSD license. See the LICENSE file for details.
 """
-from __future__ import absolute_import
+from __future__ import absolute_import, print_function
 
 import copy
 import glob
