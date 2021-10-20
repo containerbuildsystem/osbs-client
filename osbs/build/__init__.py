caed48a216db52dd3e1f8545a0007f3fdcd0b36b
Robert Cerven <rcerven@redhat.com>
2021-10-20 19:57:38 +0200

    remove build_requestv2.py, core.py, build_response.py, config_map_response.py, pod_response.py
    * CLOUDBLD-7306
    
    Signed-off-by: Robert Cerven <rcerven@redhat.com>

--- osbs/build/__init__.py
+++ osbs/build/__init__.py
@@ -6,6 +6,3 @@ This software may be modified and distributed under the terms
 of the BSD license. See the LICENSE file for details.
 """
 from __future__ import absolute_import
-
-from .build_response import BuildResponse  # noqa
-from .pod_response import PodResponse  # noqa
