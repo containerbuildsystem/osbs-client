601a8a6d6974d8a6e355479c6238e8d76ed422cc
Robert Cerven <rcerven@redhat.com>
2021-11-10 16:17:05 +0100

    Silent pylint
    * CLOUDBLD-7764
    
    Signed-off-by: Robert Cerven <rcerven@redhat.com>

--- osbs/cli/render.py
+++ osbs/cli/render.py
@@ -153,8 +153,8 @@ class TablePrinter(TableFormatter):
 
         self.header_format_str = "+".join(header_sepa_format_list)
         self.header_data = {}
-        for k in self.col_widths:
-            self.header_data[k] = "-" * self.col_widths[k]
+        for col, wid in self.col_widths.items():
+            self.header_data[col] = "-" * wid
 
     def get_all_longest_col_lengths(self):
         """
